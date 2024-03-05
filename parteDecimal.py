dd7f9eaeec2f56b262e00ffb3d2a470e1c0c47f1
DanielAcevedoVega <antonydany236@gmail.com>
2024-03-05 14:37:17 -0500

    Ejercicio 9 nota que necesito

--- parteDecimal.py
+++
@@ -1,7 +0,0 @@
-#Escriba un programa que entregue la parte decimal de un número real ingresado por el usuario.
-
-numero = float(input("Ingrese un numero: "))
-
-numero_decimal = abs(numero) - abs(int(numero))
-
-print(f"{numero_decimal:.2f}")
