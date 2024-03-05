2f89c63bfb60019291dc3ec4b87fba8fa85d9f39
DanielAcevedoVega <antonydany236@gmail.com>
2024-03-05 12:53:05 -0500

    Ejercicio 6 hipotenusa

--- numeroInvertido.py
+++ numeroInvertido.py
@@ -1,11 +1,10 @@
-#Escriba un programa que pida al usuario un entero de tres dígitos, y entregue el número con los dígitos en orden inverso:longitud_cm = int(input("Ingrese longitud en cm: "))
-numero = int(input("Ingrese un numero de tres digitos: "))
+#Escriba un programa que reciba como entrada las longitudes de los dos catetos y que entregue como salida el largo de la hipotenusa
+from math import sqrt
 
-if (numero > 99 and numero < 1000):
-    numeroInvertido = str(numero)[::-1]
-    print(numeroInvertido)
-else:
-    print("Error el numero tiene que ser de solo tres digitos")
+cateto_a = int(input("Ingrese cateto a: "))
+cateto_b = int(input("Ingrese cateto b: "))
 
+hipotenusa = sqrt((cateto_a)**2 + (cateto_b)**2)
 
+print(f"La hipotenusa es: {hipotenusa}")
     
