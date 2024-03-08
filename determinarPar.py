34fdec5726e87f21f0e3ac752404e3ac8c30c8da
DanielAcevedoVega <antonydany236@gmail.com>
2024-03-07 21:18:28 -0500

    ejercicio de año bisiesto

--- determinarPar.py
+++
@@ -1,9 +0,0 @@
-#Escriba un programa que determine si el número entero ingresado por el usuario es par o no.
-numero = int(input("Ingrese un numero: "))
-
-if numero % 2 == 0:
-    print("Su numero es par")
-else:
-    print("Su numero es impar")
-
-    
